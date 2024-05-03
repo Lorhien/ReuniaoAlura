042eb764189c40c441c809bee638adfd5f1489f4
Lorhien <142899177+Lorhien@users.noreply.github.com>
2024-05-03 11:12:15 -0300

    Create ReuniaoAlura.js
    Criei

---
+++ ReuniaoAlura.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  textSize(35);
+  text('ASCII', 150, 200,);
+  stroke(0);
+  strokeWeight(5);
+  fill('red');
+  }
