453719fecb03cd619dca30834a5b09ada59fa369
Lorhien <142899177+Lorhien@users.noreply.github.com>
2024-05-17 10:39:02 -0300

    adicionar sketch

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  textSize(35);
+  text('ASCII', 150, 200,);
+  stroke(0);
+  strokeWeight(5);
+  fill('red');
+  
+}
